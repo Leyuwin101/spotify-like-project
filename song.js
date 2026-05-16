ad89572023864bcd00b2c9c9efe284404bc47b8e
leeuwindave732-lang <leeuwindave732@gmail.com>
2026-05-16 20:09:44 +0800

    Hope it works

--- song.js
+++ song.js
@@ -16,37 +16,37 @@ export const songs = [
         title: "About You",
         artist: "The 1975",
         album: "Being Funny in a Foreign Language",
-        src: "/music/aboutyou.mp3",
-        cover: "/image/Aboutyou.jpg"
+        src: "src/music/aboutyou.mp3",
+        cover: "src/image/Aboutyou.jpg"
     },
         {
         title: "About You",
         artist: "The 1975",
         album: "Being Funny in a Foreign Language",
-        src: "/music/aboutyou.mp3",
-        cover: "/image/Aboutyou.jpg"
+        src: "src/music/aboutyou.mp3",
+        cover: "src/image/Aboutyou.jpg"
+    },
+    {
+        title: "About You",
+        artist: "The 1975",
+        album: "Being Funny in a Foreign Language",
+        src: "src/music/aboutyou.mp3",
+        cover: "src/image/Aboutyou.jpg"
     },
         {
         title: "About You",
         artist: "The 1975",
         album: "Being Funny in a Foreign Language",
-        src: "/music/aboutyou.mp3",
-        cover: "/image/Aboutyou.jpg"
-    },
-        {
-        title: "About You",
-        artist: "The 1975",
-        album: "Being Funny in a Foreign Language",
-        src: "/music/aboutyou.mp3",
-        cover: "/image/Aboutyou.jpg"
+        src: "src/music/aboutyou.mp3",
+        cover: "src/image/Aboutyou.jpg"
     },
 
     {
         title: "After Dark",
         artist: "Mr.Kitty",
         album: "Time",
-        src: "/music/another.mp3",
-        cover: "/image/afterdark.jpg"
+        src: "src/music/another.mp3",
+        cover: "src/image/afterdark.jpg"
     }
 
 ];
